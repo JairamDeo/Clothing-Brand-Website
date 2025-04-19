5f4b90b490741fe38c4e5acfa7f5fa5bd87d0b7c
Jairam Deo <jairamdeo2002@gmail.com>
2025-04-19 17:14:55 +0530

    lazy  and suspense added optimisetion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,41 @@
-import './App.css'
+import './App.css';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 } from "react-router-dom";
 
+import React, { Suspense, lazy } from 'react';
+
+// Static components
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 
-import Ladies from './Screens/Ladies';
-import Men from './Screens/Men';
-import Home from './Screens/Home';
-import Kids from './Screens/Kids';
-import AuthPage from './Authentication/AuthPage';
+// Lazy-loaded screens
+const Ladies = lazy(() => import('./Screens/Ladies'));
+const Men = lazy(() => import('./Screens/Men'));
+const Home = lazy(() => import('./Screens/Home'));
+const Kids = lazy(() => import('./Screens/Kids'));
+const AuthPage = lazy(() => import('./Authentication/AuthPage'));
 
 function App() {
-
   return (
     <Router>
       <div>
-      <Navbar/>  
-        <Routes>
-          <Route exact path='/' element={<Ladies/>}/>
-          <Route exact path='/men' element={<Men/>}/>
-          <Route exact path='/home' element={<Home/>}/>
-          <Route exact path='/kids' element={<Kids/>}/>
-          <Route exact path='/auth' element={<AuthPage/>}/>
-        </Routes>
-      <Footer/>   
+        <Navbar />
+        <Suspense fallback={<div className="text-center p-4">Loading...</div>}>
+          <Routes>
+            <Route exact path='/' element={<Ladies />} />
+            <Route exact path='/men' element={<Men />} />
+            <Route exact path='/home' element={<Home />} />
+            <Route exact path='/kids' element={<Kids />} />
+            <Route exact path='/auth' element={<AuthPage />} />
+          </Routes>
+        </Suspense>
+        <Footer />
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
